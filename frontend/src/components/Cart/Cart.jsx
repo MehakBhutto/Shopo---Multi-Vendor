0b18f7e92b6c867d6c581133dacd05d4cc9ec565
MehakBhutto <mehakbhutto842@gmail.com>
2026-07-12 03:52:59 +0500

    Add Cart component with sample data
    Created a Cart component with sample cart data.

---
+++ frontend/src/components/Cart/Cart.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const Cart = () => {
+
+  const cartData ° [
+  {
+    name : "Iphone 14 pro max 256 gb ssd and 8gb ram silver colour",
+    description: "test"
+    price: 999,
+  },
+  {
+    name : "Iphone 14 pro max 256 gb ssd and 8gb ram silver colour",
+    description: "test"
+    price: 245,
+  },
+    {
+    name : "Iphone 14 pro max 256 gb ssd and 8gb ram silver colour",
+    description: "test"
+    price: 645,
+  },
+  ];
+  
+  return (
+    <div className='fixed top-0 right-0 w-full bg-[#000004b h-screen z-10'>
+    
+    </div>
+  )
+}
