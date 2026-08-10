33ec2ec63d340bc3b6b6dcb504ea66d3c5c58d9b
MehakBhutto <mehakbhutto842@gmail.com>
2026-08-10 21:58:42 +0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,11 @@ const connectDB = require('./config/mongodb')
 
 dotenv.config();
 connectDB()
+const mongoose = require('mongoose');
+console.log('MONGO_URI present?', !!process.env.MONGO_URI);
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => { console.log('connected'); process.exit(0); })
+  .catch(err => { console.error('connect error', err); process.exit(1); });
 const app = require('./app')
 const PORT = process.env.PORT;
 
