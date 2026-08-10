303df40a46cef50bd8bb017532a9489c900d10ea
MehakBhutto <mehakbhutto842@gmail.com>
2026-08-10 16:32:35 +0500

    Update server.js

--- frontend/server.js
+++ frontend/server.js
@@ -1,2 +1,2 @@
-export const server = "http://localhost:4000/api/v2";
-export const backend_url = 'http://localhost:4000/'
+export const server = "https://shopo-multi-vendor-backend.vercel.app/api/v2";
+export const backend_url = 'https://shopo-multi-vendor-backend.vercel.app/'
