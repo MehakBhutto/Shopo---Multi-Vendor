1386f4cec655caf882463b96299efb2e8e4c97da
MehakBhutto <mehakbhutto842@gmail.com>
2026-08-10 18:52:55 +0500

    Update mongodb.js

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -9,7 +9,7 @@ const connectDB = async () => {
         console.log("MongoDB connected successfully");
     });
 
-    await mongoose.connect(process.env.MONGO_URI, {
+    await mongoose.connect("mongodb://mhkfb15_db_user:mfbhutto2186@ac-iusu4t9-shard-00-00.erg46sh.mongodb.net:27017,ac-iusu4t9-shard-00-01.erg46sh.mongodb.net:27017,ac-iusu4t9-shard-00-02.erg46sh.mongodb.net:27017/test?ssl=true&replicaSet=atlas-8cepq6-shard-0&authSource=admin&appName=Cluster0", {
         serverSelectionTimeoutMS: 10000,
     });
 }
