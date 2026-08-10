7e65bf13f939f26b94055e3268421ea2d83ce56c
MehakBhutto <mehakbhutto842@gmail.com>
2026-08-10 17:41:57 +0500

    Remove CORS origin restriction in app.js

--- backend/app.js
+++ backend/app.js
@@ -7,10 +7,7 @@ const cors = require('cors');
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({
-    origin: "https://shopo-multi-vendor.vercel.app",
-    credentials: true,
-}));
+app.use(cors());
 app.use("/", express.static("uploads"));
 app.use(bodyParser.urlencoded({extended:true, limit:'50mb'}))
 
