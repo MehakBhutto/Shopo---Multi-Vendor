ea701f8e46423545ee473511d6789bfaab1109d2
MehakBhutto <mehakbhutto842@gmail.com>
2026-08-12 00:49:26 +0500

    Enhance sendToken function with environment checks
    Added production environment checks for cookie options.

--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -1,20 +1,21 @@
-const sendToken = (user, statusCode, res) => {
+//create token and saving that n cookies
+const sendToken = ( user, statusCode, res) => {
     const token = user.getJwtToken();
+    const isProduction = process.env.NODE_ENV === "production" || process.env.VERCEL === "1";
 
     const options = {
         expires: new Date(Date.now() + 90 * 24 * 60 * 60 * 1000),
         httpOnly: true,
-        sameSite: "none",
-        secure: false,
+        sameSite: isProduction ? "none" : "lax",
+        secure: isProduction,
         path: "/",
     };
 
-    res.status(statusCode)
-        .cookie("token", token, options)
-        .json({
-            success: true,
-            user,
-        });
+    res.status(statusCode).cookie("token", token, options).json({
+        success: true,
+        user,
+        token
+    });
 };
 
 module.exports = sendToken;
