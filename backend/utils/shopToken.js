635e8d0414ec9d920fe5c307367f0515a0f66dfd
MehakBhutto <mehakbhutto842@gmail.com>
2026-08-12 00:39:57 +0500

    Update shopToken.js

--- backend/utils/shopToken.js
+++ backend/utils/shopToken.js
@@ -1,20 +1,21 @@
-//create token and saving that n cookies
-const sendShopToken = ( user, statusCode, res) => {
+const sendShopToken = (user, statusCode, res) => {
     const token = user.getJwtToken();
 
-    //Options for cookies
     const options = {
         expires: new Date(Date.now() + 90 * 24 * 60 * 60 * 1000),
         httpOnly: true,
         sameSite: "none",
         secure: true,
+        path: "/",
     };
 
-    res.status(statusCode).cookie("seller_token", token, options).json({
-        success: true,
-        user,
-        token
-    });
+    res.status(statusCode)
+        .cookie("seller_token", token, options)
+        .json({
+            success: true,
+            user,
+            token,
+        });
 };
 
 module.exports = sendShopToken;
