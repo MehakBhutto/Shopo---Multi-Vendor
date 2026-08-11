ef4db4dc71fecd0fafb9152d58649a701b32be41
MehakBhutto <mehakbhutto842@gmail.com>
2026-08-11 23:43:04 +0500

    Integrate MongoDB and enhance static file handling
    Added MongoDB connection and improved static file serving.

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,8 @@ const app = express();
 const cookieParser = require('cookie-parser');
 const bodyParser = require("body-parser");
 const cors = require('cors');
+const connectDB = require('./config/mongodb');
+const path = require('path');
 
 app.use(express.json());
 app.use(cookieParser());app.use(express.json());
@@ -12,8 +14,17 @@ app.use(cors({
     origin: ["https://shopo-multi-vendor.vercel.app","http://localhost:5173"],
     credentials: true,
 }));
-app.use("/", express.static("uploads"));
+app.use("/", express.static(path.join(__dirname, "uploads")));
 app.use(bodyParser.urlencoded({extended:true, limit:'50mb'}))
+
+app.use(async (req, res, next) => {
+    try {
+        await connectDB();
+        next();
+    } catch (error) {
+        next(error);
+    }
+});
 
 //import routes
 
