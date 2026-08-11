84e88641556ec7bc651ff42f86e934a403b8170c
MehakBhutto <mehakbhutto842@gmail.com>
2026-08-12 00:49:53 +0500

    Refactor sendShopToken to improve cookie security
    Updated cookie options to use environment variables for security.

--- backend/utils/shopToken.js
+++ backend/utils/shopToken.js
@@ -1,21 +1,20 @@
-const sendShopToken = (user, statusCode, res) => {
+//create token and saving that n cookies
+const sendShopToken = ( user, statusCode, res) => {
     const token = user.getJwtToken();
 
+    //Options for cookies
     const options = {
         expires: new Date(Date.now() + 90 * 24 * 60 * 60 * 1000),
         httpOnly: true,
-        sameSite: "none",
-        secure: true,
-        path: "/",
+        sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+        secure: process.env.NODE_ENV === "production",
     };
 
-    res.status(statusCode)
-        .cookie("seller_token", token, options)
-        .json({
-            success: true,
-            user,
-            token,
-        });
+    res.status(statusCode).cookie("seller_token", token, options).json({
+        success: true,
+        user,
+        token
+    });
 };
 
 module.exports = sendShopToken;
