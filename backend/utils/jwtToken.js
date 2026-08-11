952821f4c279d9a38364f2220131e033c1557ddc
MehakBhutto <mehakbhutto842@gmail.com>
2026-08-12 00:27:35 +0500

    Update jwtToken.js

--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -1,21 +1,20 @@
-//create token and saving that n cookies
-const sendToken = ( user, statusCode, res) => {
+const sendToken = (user, statusCode, res) => {
     const token = user.getJwtToken();
 
-    //Options for cookies
     const options = {
-    expires: new Date(Date.now() + 90 * 24 * 60 * 60 * 1000),
-    httpOnly: true,
-    sameSite: "lax",
-    secure: true,
-    path: "/",
-};
+        expires: new Date(Date.now() + 90 * 24 * 60 * 60 * 1000),
+        httpOnly: true,
+        sameSite: "none",
+        secure: true,
+        path: "/",
+    };
 
-    res.status(statusCode).cookie("token", token, options).json({
-        success: true,
-        user,
-        token
-    });
+    res.status(statusCode)
+        .cookie("token", token, options)
+        .json({
+            success: true,
+            user,
+        });
 };
 
 module.exports = sendToken;
