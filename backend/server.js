18366bc65dc45a868fe7155345ec4beb9140464d
MehakBhutto <mehakbhutto842@gmail.com>
2026-08-11 23:41:29 +0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,37 @@
 const dotenv = require('dotenv');
 const connectDB = require('./config/mongodb')
 
-
 dotenv.config();
-await connectDB()
-const app = require('./app');
+const app = require('./app')
+const PORT = process.env.PORT;
 
 //handling ncaught Exception
 process.on("uncaughtException", (err) => {
     console.log(`Error: ${err.message}`)
 })
 
-const server = app.listen(PORT, async () => {
-    await connectDB()
-    const app = require('./app');
-    console.log('Server is running on Port: ' + PORT)
+let server;
+
+const startServer = async () => {
+    await connectDB();
+
+    server = app.listen(PORT, () => {
+        console.log('Server is running on Port: ' + PORT)
+    });
+};
+
+startServer().catch((err) => {
+    console.log(`Failed to start server: ${err.message}`);
+    process.exit(1);
 });
 
 //undefined promise rejecton
 process.on("unhandledRejection", (err) => {
     console.log(`Shutting down the derver for ${err.message}`);
+
+    if (!server) {
+        process.exit(1);
+    }
 
     server.close(() => {
         process.exit(1);
